8dcf258705dc075fc80d2d987b9d11703d92f640
Ahmed Hamdy <ahmed.gomaa2703@gmail.com>
2024-06-02 23:13:41 +0300

    initial commit

---
+++ giveaways/giveawaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  giveawaysApp.swift
+//  giveaways
+//
+//  Created by Ahmed Hamdy on 02/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct giveawaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
